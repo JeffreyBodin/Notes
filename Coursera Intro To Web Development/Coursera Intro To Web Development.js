349105575123e520ad7c301f4d80ba1874d1415b
Jeffrey Bodin <jeffreybodin713@gmail.com>
2018-02-17 13:25:50 -0600

    Added notes - Altered/Added to html1   document.

--- Coursera Intro To Web Development/Coursera Intro To Web Development.js
+++ Coursera Intro To Web Development/Coursera Intro To Web Development.js
@@ -7,3 +7,29 @@
 // Each piece of content is hosted on its own address, its own page.
 // HTML = Hypertext Markup Language
 // CSS = Cascading Style Sheets 
+
+// Creating Your Own HTLM
+// Tag = thing in <>
+// HTML Tags
+<html></html>
+// Required to validate every webpage. They enclose the entire html document following the the <!DOCTYPE html> tag until the end of the code.
+// Head Tag
+<head></head>
+// Required to validate every webpage.
+// Title Tag
+<title></title>
+// Required to validate every webpage.
+// Goes inside of the <head></head> tag. Is the title of the webpage. Displays in the tab on top of webbrowser ie chrome tab's text.
+// Body Tag
+<body></body>
+// Required to validate every webpage.
+// Heading tag
+<h1></h1>
+<h2></h2>
+// Header tags can be incremented as needed. Ex h1, h2, h3 etc.
+// Each subsequent heading decreases in size inversely proportional to its designated header number.
+// Ie header h1 seems to be around 20 font whereas h3 looks like 12 font.
+// Headers are bolded.
+// Paragraph Tag
+<p></p>
+// Is not large. Is not bolded by defualt.
