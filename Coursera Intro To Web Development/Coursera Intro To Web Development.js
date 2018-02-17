b9c1ac3a30d249cc8cf175eeadd6bc8705496eeb
Jeffrey Bodin <jeffreybodin713@gmail.com>
2018-02-17 14:08:34 -0600

    Changes - Added notes on html tags and attributes. - Added new html2.html file. -- Added rick and morty THICC copypasta text. -- To use/edit as reference text.

--- Coursera Intro To Web Development/Coursera Intro To Web Development.js
+++ Coursera Intro To Web Development/Coursera Intro To Web Development.js
@@ -10,7 +10,14 @@
 
 // Creating Your Own HTLM
 // Tag = thing in <>
-// HTML Tags
+// Tag Attributes
+// Tags can have attributes they give browser additional information. Alter other html tags. 
+// href
+href = ''
+<a href = ''></a>
+<a href="http://www.google.com">Link To Google</a>
+// href specifies a link. Ie a link to google.
+// HTML Tag
 <html></html>
 // Required to validate every webpage. They enclose the entire html document following the the <!DOCTYPE html> tag until the end of the code.
 // Head Tag
@@ -33,3 +40,26 @@
 // Paragraph Tag
 <p></p>
 // Is not large. Is not bolded by defualt.
+// Will wrap text automatically by defualt.
+// Anchor Tag
+<a></a>
+<a href = ''></a>
+// Break Tag
+<br></> 
+// Adds a paragraph/line/whitespace break. 
+// Goes within the another tag containg text where whitespace/break required. 
+// No closing tag.
+// Strong Tag
+<strong></strong>
+<strong>山卄卂丅 下口口乚丂.. 卄口山 工 尸工丅丫 丅卄乇从. </strong>
+// Bolds text that it surrounds.
+// Use this tag instead b/c best practise.
+// Bold "b" Tag
+<b></b>
+<b>丫口凵 卄卂リ乇 丅口 卄卂リ乇 卂 リ乇尺丫 卄工厶卄 工㔿 丅口 凵𠘨刀乇尺丂丅卂𠘨刀 尺工匚长 卂𠘨刀 从口尺丅丫.</b> 
+// Bolds text that it surrounds.
+// b tag is being deprecated in some browsers. Use strong tag instead. 
+// Emphasis Tag
+<em></em>
+<em>丂从工尺长工𠘨厶</em>
+// Italicises text that it surrounds.
