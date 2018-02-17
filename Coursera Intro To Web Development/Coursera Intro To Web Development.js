9d734318441d2919b5266f4f7a6e87696699d976
Jeffrey Bodin <jeffreybodin713@gmail.com>
2018-02-17 13:01:04 -0600

    Moved some files

---
+++ Coursera Intro To Web Development/Coursera Intro To Web Development.js
@@ -0,0 +1,9 @@
+// Module 2: Designing Your Own Website
+
+// Components and Structure of a Website
+// Websites are setup structurally like a tiered setup.
+// Home Page --> Main Sections --> Subsections (content)
+// Home page of a website is also known as an index page.
+// Each piece of content is hosted on its own address, its own page.
+// HTML = Hypertext Markup Language
+// CSS = Cascading Style Sheets 
