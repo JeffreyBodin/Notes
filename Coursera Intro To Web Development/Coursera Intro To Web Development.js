6ed7f168eb7c381a8cfde1e5b6b3f932e462b053
Jeffrey Bodin <jeffreybodin713@gmail.com>
2018-02-17 21:28:16 -0600

    Notes

--- Coursera Intro To Web Development/Coursera Intro To Web Development.js
+++ Coursera Intro To Web Development/Coursera Intro To Web Development.js
@@ -10,6 +10,7 @@
 
 // Creating Your Own HTLM
 // Tag = thing in <>
+
 // Tag Attributes
 // Tags can have attributes they give browser additional information. Alter other html tags. 
 // href
@@ -17,19 +18,28 @@ href = ''
 <a href = ''></a>
 <a href="http://www.google.com">Link To Google</a>
 // href specifies a link. Ie a link to google.
+// src
+src = ''
+<img src='kizuna_ai_chibi.jpg'/>
+// src specifies the source file. Ie displayed image etc.
+
 // HTML Tag
 <html></html>
 // Required to validate every webpage. They enclose the entire html document following the the <!DOCTYPE html> tag until the end of the code.
+
 // Head Tag
 <head></head>
 // Required to validate every webpage.
+
 // Title Tag
 <title></title>
 // Required to validate every webpage.
 // Goes inside of the <head></head> tag. Is the title of the webpage. Displays in the tab on top of webbrowser ie chrome tab's text.
+
 // Body Tag
 <body></body>
 // Required to validate every webpage.
+
 // Heading tag
 <h1></h1>
 <h2></h2>
@@ -37,29 +47,41 @@ href = ''
 // Each subsequent heading decreases in size inversely proportional to its designated header number.
 // Ie header h1 seems to be around 20 font whereas h3 looks like 12 font.
 // Headers are bolded.
+
 // Paragraph Tag
 <p></p>
 // Is not large. Is not bolded by defualt.
 // Will wrap text automatically by defualt.
+
 // Anchor Tag
 <a></a>
 <a href = ''></a>
+// Marks the beginning and/or end of a hypertext link.
+
 // Break Tag
-<br></> 
+<br></>
+<br/> // Same as above break tag.  
 // Adds a paragraph/line/whitespace break. 
 // Goes within the another tag containg text where whitespace/break required. 
 // No closing tag.
+
 // Strong Tag
 <strong></strong>
 <strong>山卄卂丅 下口口乚丂.. 卄口山 工 尸工丅丫 丅卄乇从. </strong>
 // Bolds text that it surrounds.
 // Use this tag instead b/c best practise.
+
 // Bold "b" Tag
 <b></b>
 <b>丫口凵 卄卂リ乇 丅口 卄卂リ乇 卂 リ乇尺丫 卄工厶卄 工㔿 丅口 凵𠘨刀乇尺丂丅卂𠘨刀 尺工匚长 卂𠘨刀 从口尺丅丫.</b> 
 // Bolds text that it surrounds.
 // b tag is being deprecated in some browsers. Use strong tag instead. 
+
 // Emphasis Tag
 <em></em>
 <em>丂从工尺长工𠘨厶</em>
 // Italicises text that it surrounds.
+
+// Image Tag
+<img></img>
+<img src="kizuna_ai_chibi.jpg"/>
